6b2e16ad6bb5b67d09dc6a209b53ab3a9b087908
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-09-29 21:52:25 +0300

    :art: better scanlines

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <article>
   <h1 class="text-center effect-glow">
-    Rubakas<span class="bg-vt320-amber-light animate-[pulse_0.5s_ease-in-out_infinite]">&nbsp;</span>
+    Rubakas<span class="bg-vt320-amber-light animate-[pulse_0.75s_ease-in-out_infinite]">&nbsp;</span>
   </h1>
 </article>  
 
